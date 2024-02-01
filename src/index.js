b6cc568c3e866895d242398e3dadb8718937cc7c
Ryhor Volsasu <g.volosov@email.com>
2024-02-02 02:03:34 +0400

    feat: add function for program shutdown and add modul Readline Interface

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import os from 'os';
-import { getWelcome } from './app/welcome.js';
+import readline from 'readline';
+import { getWelcome, username } from './app/welcome.js';
 import { getDirectory } from './app/directory.js';
 
 process.chdir(os.homedir());
 getWelcome();
 getDirectory();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('line', (command) => {
+  switch (command) {
+    case '.exit':
+      exit();
+      break;
+  }
+});
+rl.on('close', () => exit());
+
+function exit() {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  process.exit();
+}
