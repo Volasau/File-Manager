f462bd75ec7b3bb27b2c82732a51030fba2121f2
Ryhor Volsasu <g.volosov@email.com>
2024-02-02 13:36:56 +0400

    fead: add a list of directories and files

---
+++ src/app/list.js
@@ -0,0 +1,26 @@
+import fs from 'fs/promises';
+import { getDirectory } from './directory.js';
+
+const getlist = async (directory) => {
+  try {
+    const filesAll = await fs.readdir(directory, { withFileTypes: true });
+
+    const folders = filesAll.filter((file) => file.isDirectory());
+    const files = filesAll.filter((file) => file.isFile());
+
+    const list = folders.concat(files);
+
+    console.table(
+      list.map((item) => ({
+        Name: item.name,
+        Type: item.isDirectory() ? 'directory' : 'file',
+      }))
+    );
+
+    getDirectory();
+  } catch (error) {
+    console.error('Operation failed');
+  }
+};
+
+export { getlist };
--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import os from 'os';
 import readline from 'readline';
 import { getWelcome, username } from './app/welcome.js';
 import { getDirectory } from './app/directory.js';
+import { getlist } from './app/list.js';
 
 process.chdir(os.homedir());
 getWelcome();
 getDirectory();
+
+let dirctory = process.cwd();
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.on('line', (command) => {
+rl.on('line', async (command) => {
   switch (command) {
     case '.exit':
       exit();
+      break;
+    case 'ls':
+      getlist(dirctory);
       break;
   }
 });
